212cf6b6f0f5e2b87f3de768754f464aa9bda1e5
smoralesm07-source <smoralesm07@gmail.com>
2026-08-16 11:12:40 -0400

    radar-prensa v0.2: catálogo territorial completo

---
+++ src/radar_prensa/geo_catalog.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+"""Catálogo DPA Chile v0.2.
+
+Derivado del catálogo geográfico probado en Monitor UAF. Se mantiene copiado
+dentro de Radar Prensa para que el radar sea reproducible e independiente.
+Cobertura esperada: 16 regiones, 56 provincias y 346 comunas.
+"""
+
+REGION_ALIASES = {
+    "arica y parinacota": "Arica y Parinacota", "tarapaca": "Tarapacá",
+    "antofagasta": "Antofagasta", "atacama": "Atacama", "coquimbo": "Coquimbo",
+    "valparaiso": "Valparaíso", "metropolitana": "Metropolitana de Santiago",
+    "region metropolitana": "Metropolitana de Santiago", "metropolitana de santiago": "Metropolitana de Santiago",
+    "ohiggins": "Libertador General Bernardo O'Higgins", "o higgins": "Libertador General Bernardo O'Higgins",
+    "libertador general bernardo ohiggins": "Libertador General Bernardo O'Higgins",
+    "maule": "Maule", "nuble": "Ñuble", "biobio": "Biobío", "araucania": "La Araucanía",
+    "la araucania": "La Araucanía", "los rios": "Los Ríos", "los lagos": "Los Lagos",
+    "aysen": "Aysén del General Carlos Ibáñez del Campo", "aisen": "Aysén del General Carlos Ibáñez del Campo",
+    "aysen del general carlos ibanez del campo": "Aysén del General Carlos Ibáñez del Campo",
+    "magallanes": "Magallanes y de la Antártica Chilena",
+    "magallanes y de la antartica chilena": "Magallanes y de la Antártica Chilena",
+}
+
+COMUNAS_POR_REGION = {
+    "Arica y Parinacota": ["Arica", "Camarones", "Putre", "General Lagos"],
+    "Tarapacá": ["Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"],
+    "Antofagasta": ["Antofagasta", "Mejillones", "Sierra Gorda", "Taltal", "Calama", "Ollagüe", "San Pedro de Atacama", "Tocopilla", "María Elena"],
+    "Atacama": ["Copiapó", "Caldera", "Tierra Amarilla", "Chañaral", "Diego de Almagro", "Vallenar", "Alto del Carmen", "Freirina", "Huasco"],
+    "Coquimbo": ["La Serena", "Coquimbo", "Andacollo", "La Higuera", "Paihuano", "Vicuña", "Illapel", "Canela", "Los Vilos", "Salamanca", "Ovalle", "Combarbalá", "Monte Patria", "Punitaqui", "Río Hurtado"],
+    "Valparaíso": ["Valparaíso", "Casablanca", "Concón", "Juan Fernández", "Puchuncaví", "Quintero", "Viña del Mar", "Isla de Pascua", "Los Andes", "Calle Larga", "Rinconada", "San Esteban", "La Ligua", "Cabildo", "Papudo", "Petorca", "Zapallar", "Quillota", "La Calera", "Hijuelas", "La Cruz", "Nogales", "San Antonio", "Algarrobo", "Cartagena", "El Quisco", "El Tabo", "Santo Domingo", "San Felipe", "Catemu", "Llaillay", "Panquehue", "Putaendo", "Santa María", "Quilpué", "Limache", "Olmué", "Villa Alemana"],
+    "Metropolitana de Santiago": ["Santiago", "Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "Tiltil", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"],
+    "Libertador General Bernardo O'Higgins": ["Rancagua", "Codegua", "Coinco", "Coltauco", "Doñihue", "Graneros", "Las Cabras", "Machalí", "Malloa", "Mostazal", "Olivar", "Peumo", "Pichidegua", "Quinta de Tilcoco", "Rengo", "Requínoa", "San Vicente", "Pichilemu", "La Estrella", "Litueche", "Marchihue", "Navidad", "Paredones", "San Fernando", "Chépica", "Chimbarongo", "Lolol", "Nancagua", "Palmilla", "Peralillo", "Placilla", "Pumanque", "Santa Cruz"],
+    "Maule": ["Talca", "Constitución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "Retiro", "San Javier", "Villa Alegre", "Yerbas Buenas"],
+    "Ñuble": ["Chillán", "Bulnes", "Chillán Viejo", "El Carmen", "Pemuco", "Pinto", "Quillón", "San Ignacio", "Yungay", "Quirihue", "Cobquecura", "Coelemu", "Ninhue", "Portezuelo", "Ránquil", "Trehuaco", "San Carlos", "Coihueco", "Ñiquén", "San Fabián", "San Nicolás"],
+    "Biobío": ["Concepción", "Coronel", "Chiguayante", "Florida", "Hualqui", "Lota", "Penco", "San Pedro de la Paz", "Santa Juana", "Talcahuano", "Tomé", "Hualpén", "Lebu", "Arauco", "Cañete", "Contulmo", "Curanilahue", "Los Álamos", "Tirúa", "Los Ángeles", "Antuco", "Cabrero", "Laja", "Mulchén", "Nacimiento", "Negrete", "Quilaco", "Quilleco", "San Rosendo", "Santa Bárbara", "Tucapel", "Yumbel", "Alto Biobío"],
+    "La Araucanía": ["Temuco", "Carahue", "Cunco", "Curarrehue", "Freire", "Galvarino", "Gorbea", "Lautaro", "Loncoche", "Melipeuco", "Nueva Imperial", "Padre Las Casas", "Perquenco", "Pitrufquén", "Pucón", "Saavedra", "Teodoro Schmidt", "Toltén", "Vilcún", "Villarrica", "Cholchol", "Angol", "Collipulli", "Curacautín", "Ercilla", "Lonquimay", "Los Sauces", "Lumaco", "Purén", "Renaico", "Traiguén", "Victoria"],
+    "Los Ríos": ["Valdivia", "Corral", "Lanco", "Los Lagos", "Máfil", "Mariquina", "Paillaco", "Panguipulli", "La Unión", "Futrono", "Lago Ranco", "Río Bueno"],
+    "Los Lagos": ["Puerto Montt", "Calbuco", "Cochamó", "Fresia", "Frutillar", "Los Muermos", "Llanquihue", "Maullín", "Puerto Varas", "Castro", "Ancud", "Chonchi", "Curaco de Vélez", "Dalcahue", "Puqueldón", "Queilén", "Quellón", "Quemchi", "Quinchao", "Osorno", "Puerto Octay", "Purranque", "Puyehue", "Río Negro", "San Juan de la Costa", "San Pablo", "Chaitén", "Futaleufú", "Hualaihué", "Palena"],
+    "Aysén del General Carlos Ibáñez del Campo": ["Coyhaique", "Lago Verde", "Aysén", "Cisnes", "Guaitecas", "Cochrane", "O'Higgins", "Tortel", "Chile Chico", "Río Ibáñez"],
+    "Magallanes y de la Antártica Chilena": ["Punta Arenas", "Laguna Blanca", "Río Verde", "San Gregorio", "Cabo de Hornos", "Antártica", "Porvenir", "Primavera", "Timaukel", "Natales", "Torres del Paine"],
+}
+
+PROVINCIAS_POR_REGION = {
+    "Arica y Parinacota": ["Arica", "Parinacota"], "Tarapacá": ["Iquique", "Tamarugal"],
+    "Antofagasta": ["Antofagasta", "El Loa", "Tocopilla"], "Atacama": ["Copiapó", "Chañaral", "Huasco"],
+    "Coquimbo": ["Elqui", "Choapa", "Limarí"],
+    "Valparaíso": ["Petorca", "Los Andes", "Marga Marga", "Quillota", "San Antonio", "San Felipe de Aconcagua", "Isla de Pascua", "Valparaíso"],
+    "Metropolitana de Santiago": ["Chacabuco", "Cordillera", "Maipo", "Melipilla", "Santiago", "Talagante"],
+    "Libertador General Bernardo O'Higgins": ["Cachapoal", "Cardenal Caro", "Colchagua"],
+    "Maule": ["Curicó", "Linares", "Talca", "Cauquenes"], "Ñuble": ["Diguillín", "Itata", "Punilla"],
+    "Biobío": ["Arauco", "Biobío", "Concepción"], "La Araucanía": ["Cautín", "Malleco"],
+    "Los Ríos": ["Valdivia", "Ranco"], "Los Lagos": ["Chiloé", "Llanquihue", "Osorno", "Palena"],
+    "Aysén del General Carlos Ibáñez del Campo": ["Coyhaique", "Aysén", "Capitán Prat", "General Carrera"],
+    "Magallanes y de la Antártica Chilena": ["Antártica Chilena", "Magallanes", "Tierra del Fuego", "Última Esperanza"],
+}
+
+REGIONES = list(COMUNAS_POR_REGION)
+COMUNAS = [c for values in COMUNAS_POR_REGION.values() for c in values]
+PROVINCIAS = [p for values in PROVINCIAS_POR_REGION.values() for p in values]
+
+AMBIGUOUS_COMMUNES = {
+    "Constitución", "Independencia", "La Unión", "Los Lagos", "Maule", "Aysén", "O'Higgins",
+    "Nacimiento", "Primavera", "Retiro", "Florida", "Canela", "Pinto", "Corral", "Victoria",
+    "San Pedro", "María Elena", "Diego de Almagro", "Pedro Aguirre Cerda", "Padre Hurtado",
+    "Santa Juana", "San Ramón", "San Miguel", "Santo Domingo", "San Fernando", "San Carlos",
+    "San Nicolás", "San Fabián",
+}
+
+def catalog_counts() -> dict[str, int]:
+    return {"regions": len(REGIONES), "provinces": len(PROVINCIAS), "communes": len(COMUNAS)}
